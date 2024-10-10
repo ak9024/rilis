ccadbd54050fa41c16bdb6b1d22b946e9705aefa
Adiatma Kamarudin <adiatma.mail@gmail.com>
2024-10-10 08:19:00 +0800

    chore: simplify server.rs

--- src/ssh/server.rs
+++ src/ssh/server.rs
@@ -12,44 +12,53 @@ pub fn server(config: &config::Config, destroy: Option<bool>) {
     let mut sess = Session::new().unwrap();
     sess.set_tcp_stream(tcp);
     sess.handshake().unwrap();
-    match sess.userauth_pubkey_file(&config.ssh.username, None, Path::new(&config.ssh.key), None) {
+
+    let public_key = sess.userauth_pubkey_file(
+        &config.ssh.username,
+        Some(Path::new("/Users/adiatma/.ssh/id_ed25519.pub")),
+        Path::new(&config.ssh.key),
+        None,
+    );
+
+    match public_key {
         Ok(_) => {
             if sess.authenticated() {
-                info!("Authenticated: ssh to {}", config.ssh.address);
+                info!("Authenticated: ssh to [_]@{}", config.ssh.address);
 
-                // @TODO
-                // command option to clean up
                 match destroy {
                     Some(destroy) if destroy => {
-                        let docker_compose_down = command(&sess, "sudo docker compose down -v");
+                        let command_docker_compose_down = format!(
+                            "sudo docker compose -f {} down -v",
+                            config.docker.docker_compose
+                        );
+                        let docker_compose_down =
+                            command(&sess, command_docker_compose_down.as_str());
                         println!("{}", docker_compose_down.trim());
 
                         let docker_system_prune = command(&sess, "sudo docker system prune -f");
-                        println!("{}", docker_system_prune)
+                        println!("{}", docker_system_prune);
+
+                        let docker_ps = command(&sess, "sudo docker ps");
+                        println!("{}", docker_ps);
+
+                        let docker_image_list = command(&sess, "sudo docker images");
+                        println!("{}", docker_image_list)
                     }
                     _ => {
-                        // @NOTE
-                        // Check docker exists or not.
                         let docker_version = command(&sess, "docker --version");
                         println!("{}", docker_version);
 
-                        // @NOTE
-                        // Install docker and docker-compose
                         if !docker_version.contains("Docker version") {
                             let install_docker = command(&sess, INSTALL_DOCKER);
                             println!("{}", install_docker.trim())
                         }
 
-                        // @NOTE
-                        // Upload file from local to the server.
                         scp(
                             &sess,
                             config.docker.docker_compose.as_str(),
                             config.docker.docker_compose.as_str(),
                         );
 
-                        // @NOTE
-                        // execute docker compose up
                         let command_docker_compose_up = format!(
                             "sudo docker compose -f {} up -d",
                             config.docker.docker_compose
@@ -57,10 +66,8 @@ pub fn server(config: &config::Config, destroy: Option<bool>) {
                         let docker_compose_up = command(&sess, command_docker_compose_up.as_str());
                         println!("{}", docker_compose_up);
 
-                        // @NOTE
-                        // execute docker compose up
                         let docker_ps = command(&sess, "sudo docker ps");
-                        println!("{}", docker_ps)
+                        println!("{}", docker_ps);
                     }
                 }
             } else {
