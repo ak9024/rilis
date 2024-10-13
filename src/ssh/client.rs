dd3371c59308e41c53e8498cc9e26097ff8b0ffc
Adiatma Kamarudin <adiatma.mail@gmail.com>
2024-10-13 20:17:23 +0800

    chore: remove progress bar

--- src/ssh/client.rs
+++ src/ssh/client.rs
@@ -1,18 +1,10 @@
 use anyhow::Result;
 use async_trait::async_trait;
 use log::info;
-use pbr::ProgressBar;
 use russh::*;
 use russh_keys::*;
-use std::{
-    path::Path,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
-    time::Duration,
-};
-use tokio::{net::ToSocketAddrs, time};
+use std::{path::Path, sync::Arc};
+use tokio::net::ToSocketAddrs;
 
 pub struct Client {}
 
@@ -74,41 +66,6 @@ impl Session {
         let mut stdout_data = Vec::new();
         let mut stderr = String::new();
 
-        // Set a dynamic refresh rate and an initial estimate for the total time
-        let refresh_rate = Duration::from_millis(100); // Progress bar refreshes every 100ms
-        let estimated_total_duration = Duration::from_secs(10); // Estimate the process to take 10 seconds (this can be adjusted)
-
-        // Calculate the total number of steps based on the refresh rate and estimated duration
-        let total_steps = (estimated_total_duration.as_millis() / refresh_rate.as_millis()) as u64;
-
-        // Create and configure the progress bar with a custom style
-        let mut pb = ProgressBar::new(total_steps);
-        pb.format("[=>-]"); // Custom format: [=>-]
-        pb.set_max_refresh_rate(Some(refresh_rate)); // Set refresh rate
-
-        // Atomic flag to signal when the process is complete
-        let is_done = Arc::new(AtomicBool::new(false));
-        let is_done_clone = Arc::clone(&is_done);
-
-        // Progress bar task
-        let pb_handle = tokio::spawn(async move {
-            let mut step = 0;
-
-            while !is_done_clone.load(Ordering::Relaxed) {
-                // Increment the progress bar and update percentage
-                step += 1;
-                pb.inc();
-
-                // Display dynamic percentage based on current progress
-                let percentage = (step * 100) / total_steps;
-                pb.message(&format!(" {}%", percentage));
-
-                // Wait for the next update
-                time::sleep(refresh_rate).await;
-            }
-        });
-
-        // Wait for the SSH command to process
         loop {
             let Some(msg) = channel.wait().await else {
                 break;
@@ -125,13 +82,6 @@ impl Session {
             }
         }
 
-        // Signal that the process is done
-        is_done.store(true, Ordering::Relaxed);
-
-        // Wait for the progress bar task to finish
-        let _ = pb_handle.await;
-
-        // Return the command output
         if stdout_data.is_empty() {
             Ok(stderr)
         } else {
