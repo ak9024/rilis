a86cbfdadb6193dc15cd5d51f170b8c621ed46f5
Adiatma Kamarudin <adiatma.mail@gmail.com>
2024-10-27 11:59:44 +0800

    chore: add brand, version and help

--- src/args.rs
+++ src/args.rs
@@ -1,7 +1,21 @@
 use clap::Parser;
 use std::path::PathBuf;
 
+const BRAND: &str = r#"
+  __            
+ /  | / / /     
+(___|  (    ___ 
+|\   | | | |___ 
+| \  | | |  __/ 
+                
+"#;
+
+pub fn print_brand() {
+    println!("{}", BRAND)
+}
+
 #[derive(Parser, Debug)]
+#[clap(version, author = clap::crate_authors!("\n"), about)]
 pub struct Args {
     #[arg(long = "cfg")]
     pub config: PathBuf,
